6bd63d6f4d241b8de6caff923202fd97192336d9
sngynhy <sngynhy@gmail.com>
2026-04-11 21:11:14 +0900

    에러 핸들링 > error.tsx, not-fount.tsx로 처리리

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,26 +1,35 @@
-import Form from '@/app/ui/invoices/edit-form';
-import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
-import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+import Form from '@/app/ui/invoices/edit-form'
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs'
+import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data'
+import { notFound } from 'next/navigation'
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
-    const params = await props.params;
-    const id = params.id;
+  const params = await props.params
+  const id = params.id
 
-    const [invoice, customers] = await Promise.all([
-        fetchInvoiceById(id),
-        fetchCustomers(),
-    ]);
-    return (
-        <main>
-            <Breadcrumbs breadcrumbs={[
-                { label: 'Invoices', href: '/dashboard/invoices' },
-                {
-                    label: 'Edit Invoice',
-                    href: `/dashboard/invoices/${id}/edit`,
-                    active: true,
-                },
-            ]} />
-            <Form invoice={invoice} customers={customers} />
-        </main>
-    );
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ])
+
+  if (!invoice) {
+    notFound()
+    //* notFound() 함수는 NEXT_NOT_FOUND 예외를 던지는 방식으로 동작
+    //* 해당 라우트 구간의 not-found.tsx 파일을 표시함
+  }
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Edit Invoice',
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  )
 }
--- app/lib/actions.tsx
+++ app/lib/actions.tsx
@@ -1,23 +1,23 @@
-'use server'; // 서버 컴포넌트
-import { z } from 'zod';
-import { revalidatePath } from 'next/cache';
-import postgres from 'postgres';
-import { redirect } from 'next/navigation';
+'use server' // 서버 컴포넌트
+import { z } from 'zod'
+import { revalidatePath } from 'next/cache'
+import postgres from 'postgres'
+import { redirect } from 'next/navigation'
 
 //* DB 연결
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' })
 
 //* 데이터 스키마 정의
 const FormSchema = z.object({
-    id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(), // coerce: 데이터 타입 변환 > 숫자형으로
-    status: z.enum(['pending', 'paid']),
-    date: z.string(),
-});
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(), // coerce: 데이터 타입 변환 > 숫자형으로
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+})
 
 //* 데이터 스키마에서 id, date 필드를 제외한 객체 생성 > omit() 메서드를 사용하여 특정 필드 제외
-const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const CreateInvoice = FormSchema.omit({ id: true, date: true })
 // id, date는 클라이언트가 입력하지 않으므로 제외 > 서버 액션에서 생성
 
 //* 서버 액션 함수 생성 > 생성 액션
@@ -25,74 +25,90 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true });
 // 해당 서버 함수는 클라이언트 및 서버 구성 요소에서 가져와 사용 가능
 // 이 파일에 포함된 함수 중 사용되지 않는 함수는 최종 앱 번들에서 자동으로 제거됨
 export async function createInvoice(formData: FormData) {
-    // form data를 객체로 변환
-    // const rawFormData = {
-    //     customerId: formData.get('customerId'),
-    //     amount: formData.get('amount'),
-    //     status: formData.get('status'),
-    // }
-    // 데이터가 많을 경우 entries() 메서드 사용
-    // for (const data of formData.entries()) {
-    //     console.log('[SERVER ACTION] Key: ', data[0], 'Value: ', data[1]);
-    // }
+  // form data를 객체로 변환
+  // const rawFormData = {
+  //     customerId: formData.get('customerId'),
+  //     amount: formData.get('amount'),
+  //     status: formData.get('status'),
+  // }
+  // 데이터가 많을 경우 entries() 메서드 사용
+  // for (const data of formData.entries()) {
+  //     console.log('[SERVER ACTION] Key: ', data[0], 'Value: ', data[1]);
+  // }
 
-    // CreateInvoice.parse(...)는 전달된 객체를 검증/변환
-    // 검증 성공 시 각 필드의 값을 추출
-    // 검증 실패 시 parse는 즉시 예외를 던짐
-    const { customerId, amount, status } = CreateInvoice.parse({
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
-    });
+  // CreateInvoice.parse(...)는 전달된 객체를 검증/변환
+  // 검증 성공 시 각 필드의 값을 추출
+  // 검증 실패 시 parse는 즉시 예외를 던짐
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  })
 
-    // 데이터 가공
-    const amountInCents = amount * 100; // 달러를 센트로 변환
-    const date = new Date().toISOString().split('T')[0]; // 송장 생성 날짜 (YYYY-MM-DD)
+  // 데이터 가공
+  const amountInCents = amount * 100 // 달러를 센트로 변환
+  const date = new Date().toISOString().split('T')[0] // 송장 생성 날짜 (YYYY-MM-DD)
 
+  try {
     // DB에 데이터 삽입
     await sql`
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
+    `
+  } catch (error) {
+    console.error('Database Error:', error)
+    // throw new Error('Failed to create invoice.')
+    return { message: 'Database Error: Failed to create invoice.' }
+  }
 
-    // 재검증 및 redirect
-    // Nextjs는 클라이언트 측 라우터 캐시를 사용하여 사용자의 브라우저에 일정 시간 동안 경로 세그먼트를 저장함
-    // 해당 캐시는 프리페칭과 함께 사용자가 경로 이동을 빠르게 할 수 있도록 하면서 서버에 대한 요청 횟수를 줄여줌
-    revalidatePath('/dashboard/invoices');
-    // DB가 업데이트되면 위 경로의 유효성을 다시 검사하고, 서버에서 최신 데이터를 가져옴
-    // 이 시점에서 사용자를 원래 페이지로 다시 리다이렉트 해야함
-    redirect('/dashboard/invoices');
+  // 재검증 및 redirect
+  // Nextjs는 클라이언트 측 라우터 캐시를 사용하여 사용자의 브라우저에 일정 시간 동안 경로 세그먼트를 저장함
+  // 해당 캐시는 프리페칭과 함께 사용자가 경로 이동을 빠르게 할 수 있도록 하면서 서버에 대한 요청 횟수를 줄여줌
+  revalidatePath('/dashboard/invoices')
+  // DB가 업데이트되면 위 경로의 유효성을 다시 검사하고, 서버에서 최신 데이터를 가져옴
+  // 이 시점에서 사용자를 원래 페이지로 다시 리다이렉트 해야함
+  redirect('/dashboard/invoices')
 }
 
 //* 데이터 스키마에서 id, date 필드를 제외한 객체 생성
-const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true })
 
 //* 서버 액션 함수 생성 > 업데이트 액션
 export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
-    });
-    const amountInCents = amount * 100;
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  })
+  const amountInCents = amount * 100
+
+  try {
     await sql`
         UPDATE invoices
         SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-        WHERE id = ${id}
-    `;
-    revalidatePath('/dashboard/invoices');
-    redirect('/dashboard/invoices');
+            WHERE id = ${id}
+    `
+  } catch (error) {
+    console.error('Database Error:', error)
+    return { message: 'Database Error: Failed to update invoice.' }
+  }
+
+  revalidatePath('/dashboard/invoices')
+  redirect('/dashboard/invoices')
+  //* redirect는 try, catch 블록 외부에서 호출해야 함
+  //* redirect가 오류를 발생시키는 방식으로 작동하기 때문
 }
 
 //* 서버 액션 함수 생성 > 삭제 액션
 export async function deleteInvoice(id: string) {
-    await sql`
+  throw new Error('Failed to delete invoice.')
+  await sql`
         DELETE FROM invoices
         WHERE id = ${id}
-    `;
-    revalidatePath('/dashboard/invoices');
+    `
+  revalidatePath('/dashboard/invoices')
 
-    // 삭제 후 페이지 리다이렉트 X
-    // 만약, 리다이렉트할 경우 삭제 후 페이지에서 삭제 버튼을 클릭할 때 삭제 액션이 다시 실행되어 오류가 발생함
-    // redirect('/dashboard/invoices');
+  // 삭제 후 페이지 리다이렉트 X
+  // 만약, 리다이렉트할 경우 삭제 후 페이지에서 삭제 버튼을 클릭할 때 삭제 액션이 다시 실행되어 오류가 발생함
+  // redirect('/dashboard/invoices');
 }
